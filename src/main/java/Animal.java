23190474befec52c84eb60182135d320bcbb53e5
Mahaha <Ma@aisbhf.com>
2025-11-11 14:51:44 +0800

    idea -commit-first

---
+++ src/main/java/Animal.java
@@ -0,0 +1,17 @@
+/**
+ * @author 小马
+ * @version 1.0
+ */
+public class Animal {
+    public static void main(String[] args) {
+        int sum = 0;
+        int count = 0;
+        for (int j = 0; j < 100; j++) {
+            System.out.println("j=" + j);
+            sum+=j;
+            count++;
+        }
+        System.out.println("sum=" + sum);
+        System.out.println("count= " + count);
+    }
+}
